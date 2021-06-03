83b25d79b277e2a2cc9798f3e04786edf906d465
Jeremy Guill <rbtm2006@me.com>
2021-06-03 00:52:29 -0700

    init

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 from notion import auth, parse_from_URL, get_database, get_page
 
-page_id = parse_from_URL('https://www.notion.so/Jerem-c82f8aa02273478991123a1738a28ecc')
+page_id = parse_from_URL('https://www.notion.so/testing-9de5b37859c8401196fdc1455cebd063')
 
 data = get_page(page_id[1])
 with open (page_id[1]+".json",'w') as file:
